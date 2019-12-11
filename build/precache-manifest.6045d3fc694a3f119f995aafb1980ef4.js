1a85faadba4a7c7764154c97b85362ddb8e20bcd
tabascospicy <rafaeltabasca@gmail.com>
2019-12-11 17:26:57 -0500

    build

---
+++ build/precache-manifest.6045d3fc694a3f119f995aafb1980ef4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe3e323dba8d8571264b89efbc8dea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1e721bc423fae994334",
+    "url": "/static/css/main.556b4a4b.chunk.css"
+  },
+  {
+    "revision": "5ebd6634583c19d13dd0",
+    "url": "/static/js/2.6f92995c.chunk.js"
+  },
+  {
+    "revision": "f1e721bc423fae994334",
+    "url": "/static/js/main.d29a8ec2.chunk.js"
+  },
+  {
+    "revision": "17cde54f009aaee72921",
+    "url": "/static/js/runtime-main.32007f3c.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "0314833123d831ab7fea21d772112343",
+    "url": "/static/media/Solway-Regular.03148331.ttf"
+  },
+  {
+    "revision": "8210cd5993c2ed3a48b70f05912d4100",
+    "url": "/static/media/yo.8210cd59.jpg"
+  }
+]);
